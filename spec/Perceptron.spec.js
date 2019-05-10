4362463507a9cf67d57865ef8d1eea09c0c75081
tireymorris <tireymorris@gmail.com>
2019-05-09 22:46:40 -0500

    Add optional biased neuron and training

--- spec/Perceptron.spec.js
+++ spec/Perceptron.spec.js
@@ -34,4 +34,34 @@ describe('Perceptron', () => {
       Math.random = random;
     });
   });
+
+  describe('train', () => {
+    test('adjusts perceptron weights during training based on target value', () => {
+      // mock Math.random to force suboptimal weights to begin with
+      const random = Math.random;
+      Math.random = jest.fn().mockImplementation(() => 1.0);
+
+      // hypothetical perceptron that returns -1 if point
+      // represented by inputs x, y, z has negative z
+      const perceptron = new Perceptron({
+        addBias: false,
+        inputs: [1.25, 1.5, -5.75], // x, y, z
+        learningRate: 0.005,
+        numWeights: 3
+      });
+
+      expect(perceptron.getOutput()).toEqual(-1);
+
+      Math.random = random;
+
+      // train for 10 rounds with small learning rate (requires more rounds)
+      for (let i = 0; i < 10; i++) {
+        perceptron.train(1);
+      }
+
+      expect(perceptron.getOutput()).toEqual(1);
+
+      Math.random = random;
+    });
+  });
 });
--- src/Perceptron.js
+++ src/Perceptron.js
@@ -2,27 +2,59 @@ import { sign } from './activation';
 import { zip } from './util';
 
 class Perceptron {
-  constructor({ numWeights = 1, inputs = [], activate = sign }) {
-    this.weights = [];
-    this.inputs = inputs;
+  constructor({
+    activate = sign,
+    addBias = true,
+    inputs = [],
+    learningRate = 0.05,
+    numWeights = 1
+  }) {
     this.activate = activate;
+    this.addBias = addBias;
+    this.inputs = inputs;
+    this.learningRate = learningRate;
+    this.weights = [];
 
     this.generateWeights(numWeights);
   }
 
   generateWeights(numWeights) {
     // Generate weights between -1 and 1
+    this.weights = [];
+
     for (let i = 0; i < numWeights; i++) {
       this.weights.push(Math.random() * 2 - 1);
+    }
+
+    if (this.addBias) {
+      this.weights.push(1);
+      this.inputs.push(1);
     }
   }
 
   getOutput() {
+    // calculate weighted sum of inputs and weights
     const sum = zip(this.inputs, this.weights).reduce(
       (acc, [input, weight]) => acc + input * weight,
       0.0
     );
+
+    // run activation function on weighted sum
+    console.log('confidence = ', sum);
     return this.activate(sum);
+  }
+
+  train(targetOutput) {
+    // retrieve current output
+    const guessedOutput = this.getOutput();
+
+    // calculate error between known expected value and guessed output
+    const error = this.learningRate * (targetOutput - guessedOutput);
+
+    // adjust weights to compensate for error
+    this.weights = this.weights.map(
+      (weight, index) => weight + error * this.inputs[index]
+    );
   }
 }
 
