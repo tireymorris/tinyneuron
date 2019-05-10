cc577e686a603cef28336a7bd3abc86f566866a1
Ty Morris <tymorris@sharethrough.com>
2019-05-10 11:11:26 -0500

    Add neural network skeleton Update package.json

---
+++ src/NeuralNetwork.js
@@ -0,0 +1,7 @@
+class NeuralNetwork {
+  constructor({ numInputs = 1, numHiddens = 1, numOutputs = 1 }) {
+    this.numInputs = numInputs;
+    this.numHiddens = numHiddens;
+    this.numOutputs = numOutputs;
+  }
+}
